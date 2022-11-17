79e7fb43bda0489ed4bee784b8112b48812c8bfd
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-11-17 20:52:04 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -8,10 +8,11 @@ describe('SignUp Controller', () => {
         // name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
+        passwordConfirmation: 'any_password',
+      },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
